f81523d3bbab071c527ba4d5b43dec9a3c784daf
Juan <c4pjackie@gmail.com>
2026-07-19 21:20:43 -0500

    Fixed redirection to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import TopNav from "@/components/TopNav";
+import SessionProvider from "@/components/SessionProvider";
 import { auth } from "@/auth";
 import { Toaster } from "sonner";
 
@@ -31,11 +32,13 @@ export default async function RootLayout({
       className={`${roboto.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Toaster position="top-right" richColors />
-        {showNav && <TopNav />}
-        <div className={showNav ? "pt-14 flex flex-col flex-1" : "flex flex-col flex-1"}>
-          {children}
-        </div>
+        <SessionProvider session={session}>
+          <Toaster position="top-right" richColors />
+          {showNav && <TopNav />}
+          <div className={showNav ? "pt-14 flex flex-col flex-1" : "flex flex-col flex-1"}>
+            {children}
+          </div>
+        </SessionProvider>
       </body>
     </html>
   );
